6219f9e0cf1567748dae458d79ccf417da63845e
tuminzee <t.sheth33@gmail.com>
2020-09-05 00:59:28 +0530

    C:/Program Files/Git/promotions Schema Models REST MongoDb

---
+++ models/promotions.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        required: true
+    },
+    price:{
+        type: Currency,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+
+var Promotions = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotions;
+
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,27 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Promotions = require('../models/promotions');
+
 const promoRouter = express.Router(); //mini express app
+
 
 promoRouter.use(bodyParser.json())
 
 promoRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next(); //to complete the middleware
-})
 .get((req,res,next) => {
-    res.end('Will send all the promotions to you!')
+    Promotions.find({})
+    .then((promotions) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotions); //Will send all the data as a json format
+
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post((req,res,next) => {
-    res.end('Will add the promo: ' + req.body.name + ' with details ' + req.body.description);
+    Promotions.create(req.body)
+    .then((promotion) => {
+        console.log('Pormotion adding to the db');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .put((req,res,next) => {
     res.statusCode = 403; //Operation not supported
-    res.end('PUT operation not supported on /promotions');
+    res.end('PUT operation not supported on /promotions');  
 })
 .delete((req,res,next) => {
-    res.end('Deleting all the promotions!')
+    Promotions.remove({})
+    .then((resp) => {
+        console.log('Deleting data from db');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 promoRouter.route('/:promoId')
