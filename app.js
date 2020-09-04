8a37191d343a69952e6c61ea69b5774a10b652b2
tuminzee <t.sheth33@gmail.com>
2020-09-05 02:33:40 +0530

    Basic Auth added with static instance

--- app.js
+++ app.js
@@ -38,7 +38,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Basic Authenication middleware implementation
+function auth(req, res, next){
+  console.log(req.headers)
+
+  var authHeader = req.headers.authorization;
+  if( !authHeader ){
+    var err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+   //Decodes the authHeader
+
+  console.log(auth);
+  var username = auth[0];
+  var password = auth[1];
+
+  if ( username === 'admin' && password === 'password'){
+    next();
+  }
+  
+  
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
