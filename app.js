6145fbb9df727449f30b089aa8a8d9b3325fe2af
tuminzee <t.sheth33@gmail.com>
2020-09-06 01:09:02 +0530

    Cookie setup

--- app.js
+++ app.js
@@ -37,37 +37,55 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321')); //signed cookie
 
 //Basic Authenication middleware implementation
 function auth(req, res, next){
-  console.log(req.headers)
+  console.log(req.signedCookies)
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
 
-  var authHeader = req.headers.authorization;
-  if( !authHeader ){
-    var err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+      if( !authHeader ){
+        var err = new Error('You are not authenticated');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+      }
+
+      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      //Decodes the authHeader
+    
+      var username = auth[0];
+      var password = auth[1];
+    
+      if ( username === 'admin' && password === 'password'){
+        res.cookie('user', 'admin', { signed: true })
+        next();
+      } 
+      else {
+          var err = new Error('You are not authenticated');
+
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+        }
+    }
+    else{
+      if(req.signedCookies.user === 'admin'){
+        next();
+      }
+      else{
+        var err = new Error('You are not authenticated');
+
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return nedxt(err);
+
+      }
+    }
   }
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-   //Decodes the authHeader
 
-  console.log(auth);
-  var username = auth[0];
-  var password = auth[1];
 
-  if ( username === 'admin' && password === 'password'){
-    next();
-  } else{
-    var err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-  
-  
-}
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
