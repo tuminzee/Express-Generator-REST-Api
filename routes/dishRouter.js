5ffefcfce81717962c4b43e76a85dcd812111e8b
tuminzee <t.sheth33@gmail.com>
2020-09-13 17:03:03 +0530

    Assignmnet 3

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -140,7 +140,7 @@ dishRouter.route('/:dishId/comments')
     res.statusCode = 403; //Operation not supported
     res.end('PUT operation not supported on /dishes/' + req.params.dishId + '/comments');
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     // res.end('Deleting all the dishes!')
     Dishes.findById(req.params.dishId)
     .then((dish) => {
@@ -194,44 +194,84 @@ dishRouter.route('/:dishId/comments/:commentId')
     res.end('POST operation not supported on /dishes/'+ req.params.dishId
         + '/comments/' + req.params.commentId);
 })
+// .put(authenticate.verifyUser, (req, res, next) => {
+//     Dishes.findById(req.params.dishId)
+//     .then((dish) => {
+//         if (dish != null && dish.comments.id(req.params.commentId) != null) {
+//             if (req.body.rating) {
+//                 dish.comments.id(req.params.commentId).rating = req.body.rating;
+//             }
+//             if (req.body.comment) {
+//                 dish.comments.id(req.params.commentId).comment = req.body.comment;                
+//             }
+//             dish.save()
+//             .then((dish) => {
+//                 Dishes.findById(dish._id)
+//                 .populate('comments.author')
+//                 .then((dish) => {
+//                     res.statusCode = 200;
+//                     res.setHeader('Content-Type', 'application/json');
+//                     res.json(dish);   
+//                 })
+//             }, (err) => next(err));
+//         }
+//         else if (dish == null) {
+//             err = new Error('Dish ' + req.params.dishId + ' not found');
+//             err.status = 404;
+//             return next(err);
+//         }
+//         else {
+//             err = new Error('Comment ' + req.params.commentId + ' not found');
+//             err.status = 404;
+//             return next(err);            
+//         }
+//     }, (err) => next(err))
+//     .catch((err) => next(err));
+// })
+
 .put(authenticate.verifyUser, (req, res, next) => {
-    Dishes.findById(req.params.dishId)
-    .then((dish) => {
-        if (dish != null && dish.comments.id(req.params.commentId) != null) {
+    Dishes.findById(req.params.dishId).then((dish) => {
+        if (dish != null && dish.comments.id(req.params.commentId)) {
+            if (dish.comments.id(req.params.commentId).author.toString() != req.user._id.toString()) {
+                err = new Error('You are not authorized to edit this comment');
+                err.status = 403;
+                return next(err);
+            }
             if (req.body.rating) {
                 dish.comments.id(req.params.commentId).rating = req.body.rating;
             }
+
             if (req.body.comment) {
-                dish.comments.id(req.params.commentId).comment = req.body.comment;                
+                dish.comments.id(req.params.commentId).comment = req.body.comment;
             }
-            dish.save()
-            .then((dish) => {
-                Dishes.findById(dish._id)
-                .populate('comments.author')
-                .then((dish) => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json(dish);   
-                })
-            }, (err) => next(err));
-        }
-        else if (dish == null) {
+            dish.save().then((dish) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(dish);
+            }, (err) => next(err)).catch((err) => next(err));
+        } else if (dish == null) {
             err = new Error('Dish ' + req.params.dishId + ' not found');
             err.status = 404;
             return next(err);
-        }
-        else {
+        } else {
             err = new Error('Comment ' + req.params.commentId + ' not found');
             err.status = 404;
-            return next(err);            
+            return next(err);
         }
-    }, (err) => next(err))
-    .catch((err) => next(err));
+    }, (err) => next(err)).catch((err) => next(err));
 })
+
 .delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
+
+            if(dish.comments.id(req.params.commentId).author.toString() != req.user._id.toString()){
+                err = new Error('You are not authorized to edit this comment');
+                err.status = 403;
+                return next(err);
+            }
+
             dish.comments.id(req.params.commentId).remove();
             dish.save()
             .then((dish) => {
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -21,7 +21,7 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.post(authenticate.verifyUser, (req,res,next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     Promotions.create(req.body)
     .then((promotion) => {
         console.log('Pormotion adding to the db');
@@ -31,11 +31,11 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.put(authenticate.verifyUser, (req,res,next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     res.statusCode = 403; //Operation not supported
     res.end('PUT operation not supported on /promotions');  
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin, (req,res,next) => {
     Promotions.remove({})
     .then((resp) => {
         console.log('Deleting data from db');
@@ -56,11 +56,11 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req,res,next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdmin, (req,res,next) => {
     res.statusCode = 403; //Operation not supported
     res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
-.put(authenticate.verifyUser, (req,res,next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin, (req,res,next) => {
    Promotions.findByIdAndUpdate(req.params.promoId, {
        $set: req.body
    }, {new:true}) //new:true will return to 
@@ -72,7 +72,7 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin, (req,res,next) => {
     Promotions.findByIdAndRemove(req.params.promoId)
     .then((resp) => {
         console.log(resp);
